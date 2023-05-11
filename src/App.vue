7d0f0a7a05e7770ef951a9d3d0639e095b2d9c33
MouadBNL <mouadbnl.2k01@gmail.com>
2023-05-11 18:44:33 +0100

    chore: added NaiveUI

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,10 @@
 <script setup lang="ts">
+import { NButton } from "naive-ui";
 </script>
 
 <template>
   <div>
-    <h1>Delivoo food</h1>
+    <h1 class="mb-4">Delivoo food</h1>
+    <NButton type="primary">Hello</NButton>
   </div>
 </template>
